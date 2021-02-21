3ebaa664f743f389fd7d3a56938c349bdace7b32
Ischerba32 <i.scherba32@gmail.com>
2021-02-21 16:19:37 +0300

    modified

--- src/js/app.js
+++ src/js/app.js
@@ -5,42 +5,40 @@ import formUI from "./views/form";
 import currencyUI from "./views/currency";
 import ticketsUI from "./views/tickets";
 
-const testTickets = [{
-  airline_name: "Белавиа",
-  airline_logo: "",
-  origin_name: "Минск",
-  destination_name: "Москва",
-  departure_at: "16.12.2020 09:00",
-  return_at: "18.12.2020 18:00",
-  price: 480,
-  transfers: 1,
-  flight_number: 452
-},{
-  airline_name: "Turkish Airline",
-  airline_logo: "",
-  origin_name: "Минск",
-  destination_name: "Москва",
-  departure_at: "18.12.2020 09:00",
-  return_at: "20.12.2020 18:00",
-  price: 580,
-  transfers: 2,
-  flight_number: 123
-},{
-  airline_name: "Polish Airlines",
-  airline_logo: "",
-  origin_name: "Минск",
-  destination_name: "Москва",
-  departure_at: "14.12.2020 09:00",
-  return_at: "16.12.2020 18:00",
-  price: 500,
-  transfers: 3,
-  flight_number: 556
-}];
+// const testTickets = [{
+//   airline_name: "Белавиа",
+//   airline_logo: "",
+//   origin_name: "Минск",
+//   destination_name: "Москва",
+//   departure_at: "16.12.2020 09:00",
+//   return_at: "18.12.2020 18:00",
+//   price: 480,
+//   transfers: 1,
+//   flight_number: 452
+// },{
+//   airline_name: "Turkish Airline",
+//   airline_logo: "",
+//   origin_name: "Минск",
+//   destination_name: "Москва",
+//   departure_at: "18.12.2020 09:00",
+//   return_at: "20.12.2020 18:00",
+//   price: 580,
+//   transfers: 2,
+//   flight_number: 123
+// },{
+//   airline_name: "Polish Airlines",
+//   airline_logo: "",
+//   origin_name: "Минск",
+//   destination_name: "Москва",
+//   departure_at: "14.12.2020 09:00",
+//   return_at: "16.12.2020 18:00",
+//   price: 500,
+//   transfers: 3,
+//   flight_number: 556
+// }];
 document.addEventListener("DOMContentLoaded", (e) => {
   const form = formUI.form;
   
-  ticketsUI.renderTickets(testTickets);
-
   const objOfAirlines = testTickets.reduce((acc, item) => {
     acc[item.airline_name] = item;
     return acc;
